8ba335e78e32fc1e63f9031349d5d9804a83327c
Matthew Kendall <47122515+mattkendall11@users.noreply.github.com>
2023-01-28 11:28:26 +0000

    Add files via upload

---
+++ src/filtering.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+data = pd.read_csv('total_dataset.csv')
+def parkinson_filter(data):
+    k_star = data['Kstar_M']
+    B0 = data[B0_M]
+    for i in tqdm(range(0,len(k_star)-1)):
+        if k_star[i] < 792 or k_star[i] > 992:
+            data.drop(i, axis = 0, inplace = True)
+        if B0[i] < 4850 or B0[i] > 5780:
+            data.drop(i, axis = 0, inplace = True)
+    data.reset_index(inplace = True)
+data = parkinson_mass_filter(data)
+print(data)
+
+
+
