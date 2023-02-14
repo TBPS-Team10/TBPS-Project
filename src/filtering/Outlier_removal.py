7311d83e2e367ff5428a1b1741b79695eb401c82
Avi Aggarwal <aviagg2002@gmail.com>
2023-02-14 11:06:33 +0000

    Update Outlier_removal.py
    Included some filters found by statistical analysis of phimumu.csv against sig.csv.

--- src/filtering/Outlier_removal.py
+++ src/filtering/Outlier_removal.py
@@ -41,6 +41,14 @@ def filter_events(signal_file, event_file):
         for col, min_value, max_value in zip(important_variables, min_values_list, max_values_list):
             df.drop(df[(df[col] < min_value) | (df[col] > max_value)].index, inplace=True)
         return df
+    
+    def other_filters(df):
+        df.drop(df[(df['B0_OWNPV_Y'] < -0.27)&(df['B0_OWNPV_Y'] > -0.09)].index, inplace = True)
+        #df.drop(df[(df['B0_M'] < 5220)&(df['B0_M'] > 5340)].index, inplace = True)
+        df.drop(df[(df['B0_ENDVERTEX_CHI2'] < 11)].index, inplace = True)
+        df.drop(df[(df['Kstar_ENDVERTEX_CHI2'] < 2)].index, inplace = True)
+        #df.drop(df[(df['Kstar_PT'] < 9500)].index, inplace = True)
+        df.drop(df[(df['B0_OWNPV_X'] < 0.75)&(df['B0_OWNPV_X'] > 0.93)].index, inplace = True)
 
     # Event filtering
     event_signal = pd.read_csv(event_file, dtype="float32", delimiter=",", skiprows=0)
@@ -48,6 +56,11 @@ def filter_events(signal_file, event_file):
 
     print(event_signal) #dataframe before filtering
     event_signal_cleaned = remove_outliers(event_signal, min_values_list, max_values_list, important_variables)
-    print(event_signal_cleaned) #dataframe after filtering
+    print(event_signal) #dataframe after filtering
+    event_signal_further_cleaned = other_filters(event_signal_cleaned)
+    print(event_signal)
+    return event_signal_further_cleaned
 
-    return event_signal_cleaned
+#%%
+#test
+#filtered_data = filter_events("../data/sig.csv","../data/total_dataset.csv")
